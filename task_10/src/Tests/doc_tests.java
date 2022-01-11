25c8806dd168605b792416e4101bfe20ef3b77bc
Dmitry2004126 <mityakorolev2004@mail.ru>
2022-01-11 16:25:11 +0300

    создан тест addDoc_addDocWithNullNumberAndNullDate_ThrowsException()

--- task_10/src/Tests/doc_tests.java
+++ task_10/src/Tests/doc_tests.java
@@ -28,4 +28,11 @@ public class doc_tests extends Assert {
         var exc = assertThrows(IllegalArgumentException.class, () -> docBook.addDoc("number",null));
         assertTrue(exc.getMessage().toLowerCase().contains("date cannot be null"));
     }
+    @Test
+    public void addDoc_addDocWithNullNumberAndNullDate_ThrowsException(){
+        DocBook docBook = DocBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> docBook.addDoc(null,null));
+        assertTrue(exc.getMessage().toLowerCase().contains("number cannot be null") &&
+                exc.getMessage().toLowerCase().contains("date cannot be null"));
+    }
 }
