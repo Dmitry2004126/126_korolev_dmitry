b60e17d1c1aa359514ddd37ad82d3df3db288e40
Dmitry2004126 <mityakorolev2004@mail.ru>
2022-01-11 15:48:07 +0300

    two tests complete

--- task_10/src/Core/DocBook.java
+++ task_10/src/Core/DocBook.java
@@ -1,10 +1,18 @@
 package Core;
 
 public class DocBook {
+    private int docCount;
+    private DocBook(){
+        docCount=0;
+    }
     public int getDocCount(){
-        return 0;
+        return docCount;
     }
     public static DocBook create(){
         return new DocBook();
+    }
+
+    public void addDoc(String number, String date) {
+        docCount++;
     }
 }
