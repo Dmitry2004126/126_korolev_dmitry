9391d1016ad25aaad78bad9ba79001cb1d2de98b
Dmitry2004126 <mityakorolev2004@mail.ru>
2022-01-11 18:33:45 +0300

    test complete

--- task_10/src/Core/Document.java
+++ task_10/src/Core/Document.java
@@ -7,33 +7,42 @@ import java.util.List;
 public class Document {
     private String date;
     private HashMap<Integer, PaymentDoc> paymentDocs;
-    public Document(String date){
+
+    public Document(String date) {
         this.date = date;
         paymentDocs = new HashMap<>();
 
     }
-    public void registerPaymentDoc(int sum, int number, TypeOfPaymentDoc type, String date){
-        paymentDocs.put(number, new PaymentDoc(sum,type, date));
+
+    public void registerPaymentDoc(int sum, int number, TypeOfPaymentDoc type, String date) {
+        paymentDocs.put(number, new PaymentDoc(sum, type, date));
     }
-    public int getPaymentDocCount(){
+
+    public int getPaymentDocCount() {
         return paymentDocs.size();
     }
-    public HashMap<Integer, PaymentDoc> getPaymentDocuments(){
+
+    public HashMap<Integer, PaymentDoc> getPaymentDocuments() {
         return paymentDocs;
     }
+
     public List<Integer> getListOfPayments() {
         List<Integer> payments = new ArrayList();
-        for(PaymentDoc paymentDoc : paymentDocs.values()){
+        for (PaymentDoc paymentDoc : paymentDocs.values()) {
             payments.add(paymentDoc.getSum());
         }
 
         return payments;
     }
+
     public int getSumOfPayments() {
-        int sum=0;
-        for(PaymentDoc doc : paymentDocs.values()){
+        int sum = 0;
+        for (PaymentDoc doc : paymentDocs.values()) {
             sum += doc.getSum();
         }
         return sum;
     }
+
+
+
 }
