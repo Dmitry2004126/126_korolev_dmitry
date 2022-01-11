66a6999bcbdde5636d1160b2e6a411037751161f
Dmitry2004126 <mityakorolev2004@mail.ru>
2022-01-11 15:29:28 +0300

    Создан тест create_CreateEmptyDocBook_DocCountEqualsZero()

--- task_10/src/Tests/doc_tests.java
+++ task_10/src/Tests/doc_tests.java
@@ -3,4 +3,10 @@ package Tests;
 import org.junit.*;
 
 public class doc_tests extends Assert {
+    @Test
+    public void create_CreateEmptyDocBook_DocCountEqualsZero(){
+        DocBook docBook = DocBook.create();
+        assertEquals(0,docBook.getDocCount());
+    }
+
 }
